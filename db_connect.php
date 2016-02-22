d39ff526d0a22bd7592412025be09b77817d86d8
Tosh Camille <tochecamille@gmail.com>
2016-02-22 19:47:02 +0100

    indentation

--- db_connect.php
+++ db_connect.php
@@ -5,4 +5,10 @@
  $bdd_user     = ""; // Username
  $bdd_password = ""; // Password
 
+try {
+	$bdd = new PDO('mysql:host='.$bdd_host.';dbname='.$bdd_db.';charset=utf8', $bdd_user, $bdd_password);
+} catch(Exception $e) {
+	// En cas d'erreur, on affiche un message et on arrête tout
+	die('Erreur : '.$e->getMessage());
+}
 ?>
