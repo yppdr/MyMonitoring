ddc9900be2697dfe4c00ce1e3840da0f2def9ee5
Tosh Camille <tochecamille@gmail.com>
2016-02-22 19:08:45 +0100

    clean

--- index.php
+++ index.php
@@ -31,25 +31,12 @@ require_once('db_connect.php');
 
 
                     </tr>
-<?php $user = $_SESSION['login']; ?>
-<?php
-try
-{
-$bdd = new PDO('mysql:host='.$bdd_host.';dbname='.$bdd_db.';charset=utf8', $bdd_user, $bdd_password);
-}
-catch(Exception $e)
-{
-
-    // En cas d'erreur, on affiche un message et on arrête tout
-
-        die('Erreur : '.$e->getMessage());
-  }
+<?php $user = $_SESSION['login']; 
 
 // On récupère tout le contenu de la table jeux_video
 $reponse = $bdd->prepare('SELECT * FROM `servers` WHERE `user`= ?');
 $reponse->execute(array($user));
-while ($donnees = $reponse->fetch())
-{
+while ($donnees = $reponse->fetch()) {
 ?>
                     <tr>
                       <td style="width:20%;"><?php echo $donnees['nom']; ?></td>
@@ -57,46 +44,37 @@ while ($donnees = $reponse->fetch())
 <?php
 $ip = $donnees['IP'];
 $port = $donnees['port'];
-if (!$socket = @fsockopen($ip, $port, $errno, $errstr, 30))
+if (!$socket = @fsockopen($ip, $port, $errno, $errstr, 30)) {
 
-{
- echo '<span class="label label-danger">Hors ligne</span>';
+  echo '<span class="label label-danger">Hors ligne</span>';
 
-
-$destinataire = $donnees['user'];
-// Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
-$expediteur = $donnees['user'];
-$objet = 'MyMonitor - Serveur Offline'; // Objet du message
-$headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
-$headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
-$headers .= 'From: "MyMonitor"<'.$expediteur.'>'."\n"; // Expediteur
-$headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
-$headers .= 'Cc: '.$copie."\n"; // Copie Cc
-$headers .= 'Bcc: '.$copie_cachee."\n\n"; // Copie cachée Bcc
-$message = 'Bonjour,
-
-Votre serveur '.$ip.' ne semble plus joignable.
-Merci de regarder au plus vite.
-MyMonitor';
-if (mail($destinataire, $objet, $message, $headers)) // Envoi du message
-{
-    echo '';
-}
-else // Non envoyé
-{
-    echo "";
-}
-
-
-}
-else
-{ echo '<span class="label label-success">En ligne</span>'; fclose($socket);
+  $destinataire = $donnees['user'];
+  // Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
+  $expediteur = $donnees['user'];
+  $objet = 'MyMonitor - Serveur Offline'; // Objet du message
+  $headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
+  $headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
+  $headers .= 'From: "MyMonitor"<'.$expediteur.'>'."\n"; // Expediteur
+  $headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
+  $headers .= 'Cc: '.$copie."\n"; // Copie Cc
+  $headers .= 'Bcc: '.$copie_cachee."\n\n"; // Copie cachée Bcc
+  $message = 'Bonjour,
+  
+  Votre serveur '.$ip.' ne semble plus joignable.
+  Merci de regarder au plus vite.
+  MyMonitor';
+  if (mail($destinataire, $objet, $message, $headers)) // Envoi du message
+  {
+      echo '';
+  }
+  else // Non envoyé
+  {
+      echo "";
+  }
+} else {
+  echo '<span class="label label-success">En ligne</span>'; fclose($socket);
 }
 ?>
-
-
-
-
 
  </td>
                       <td  style="width:50%;"><?php echo $donnees['description']; ?> </td>
@@ -106,9 +84,7 @@ else
 
 }
 
-
 $reponse->closeCursor(); // Termine le traitement de la requête
-
 
 ?>
 
